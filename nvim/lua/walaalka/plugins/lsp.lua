715b0944fff3b624c14aa0ca4632f4712ddf87ac
maxamed1st <maxamed1st@gmail.com>
2024-06-16 14:31:13 +0200

    merge completion and lsp into one file

--- nvim/lua/walaalka/plugins/lsp.lua
+++ nvim/lua/walaalka/plugins/lsp.lua
@@ -95,4 +95,35 @@ return {
   -- neodev.nvim for enhanced Lua development
   { "folke/neodev.nvim", opts = {} },
 
+  	-- set up snippets and completions
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			{ "rafamadriz/friendly-snippets" },
+			{ "L3MON4D3/LuaSnip" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+		},
+		config = function()
+			local cmp = require("cmp")
+			require("luasnip.loaders.from_vscode").lazy_load()
+
+			cmp.setup({
+				mapping = cmp.mapping.preset.insert({
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "lua_snip" },
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+		end,
+	},
 }
