77a883598f9c7383f955db4810acfa9dfd5cbe39
maxamed1st <maxamed1st@gmail.com>
2024-06-26 13:27:29 +0200

    add mapping for global diagnostics and format file

--- nvim/lua/walaalka/plugins/whichkey.lua
+++ nvim/lua/walaalka/plugins/whichkey.lua
@@ -1,48 +1,48 @@
-return {
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-    end,
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
-    config = function()
-      local wk = require("which-key")
-      local builtin = require("telescope.builtin")
-      local utils = require("walaalka.utils")
-      wk.register({
-        f = {
-          name = "Files/Buffers",
-          b = { "<cmd>Telescope buffers<cr>", "Buffers" },
-          d = { utils.delete_buffer, "delete buffer"},
-          n = { "<cmd>enew<cr>", "New File" },
-          r = { "<cmd>Telescope oldfiles<cr>", "Recent Files" },
-        },
-        i = {
-          name = "Interface",
-          c = { "<cmd>Telescope colorscheme<cr>", "Color Scheme" },
-        },
-        c = {
-          name = "Code",
-          m = { "<cmd>Mason<cr>", "Mason" },
-          f = { "<cmd>lua vim.lsp.buf.format({ async = true })<cr>", "Format" },
-        },
-        w = {
-          name = "Window",
-          d = { "<cmd>close<cr>", "delete window" },
-        },
-        s = {
-          name = "Search",
-          b = { builtin.current_buffer_fuzzy_find, "Buffer" },
-          g = { builtin.live_grep, "Live Grep" },
-        },
-        g = { name = "Git" },
-      }, { prefix = "<leader>" })
-    end,
+return
+{
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+  end,
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
   },
+  config = function()
+    local wk = require("which-key")
+    local builtin = require("telescope.builtin")
+    local utils = require("walaalka.utils")
+    wk.register({
+      f = {
+        name = "Files/Buffers",
+        b = { "<cmd>Telescope buffers<cr>", "Buffers" },
+        d = { utils.delete_buffer, "delete buffer" },
+        n = { "<cmd>enew<cr>", "New File" },
+        r = { "<cmd>Telescope oldfiles<cr>", "Recent Files" },
+      },
+      i = {
+        name = "Interface",
+        c = { "<cmd>Telescope colorscheme<cr>", "Color Scheme" },
+      },
+      c = {
+        name = "Code",
+        m = { "<cmd>Mason<cr>", "Mason" },
+        f = { "<cmd>lua vim.lsp.buf.format({ async = true })<cr>", "Format" },
+        D = { builtin.diagnostics, "Show all diagnostics" },
+      },
+      w = {
+        name = "Window",
+        d = { "<cmd>close<cr>", "delete window" },
+      },
+      s = {
+        name = "Search",
+        b = { builtin.current_buffer_fuzzy_find, "Buffer" },
+        g = { builtin.live_grep, "Live Grep" },
+      },
+      g = { name = "Git" },
+    }, { prefix = "<leader>" })
+  end,
 }
