0b16a5e904f788bbae20426717b77625c55ee87a
maxamed1st <maxamed1st@gmail.com>
2025-03-21 20:25:53 +0100

    set more shortcuts for git

--- nvim/lua/plugins/code.lua
+++ nvim/lua/plugins/code.lua
@@ -7,10 +7,12 @@ return {
     keys = {
       { "<leader>gg", "<cmd>Git<cr>", desc = "Git Status" },
       { "<leader>ga", "<cmd>Git add %<cr>", desc = "Git Add" },
+      { "<leader>gb", "<cmd>Git branch<cr>", desc = "Git Branch" },
       { "<leader>gc", "<cmd>Git commit<cr>", desc = "Git Commit" },
       { "<leader>gp", "<cmd>Git push<cr>", desc = "Git Push" },
       { "<leader>gm", "<cmd>Git commit --amend --no-edit<cr>", desc = "Git Amend" },
       { "<leader>gD", "<cmd>Git diff<cr>", desc = "Git diff all" },
+      { "<leader>gr", "<cmd>Git reflog<cr>", desc = "Git reflog" },
     },
   },
 
