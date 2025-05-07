4aa95b7e5a5e755a40b6b8dabd27fdfbfe464873
maxamed1st <maxamed1st@gmail.com>
2025-05-08 01:33:33 +0200

    set up attached buffers and capabilities in neovims lspconfig

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,7 +4,37 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      require("lspconfig")
+      local lspconfig = require("lspconfig")
+
+      local capabilities = vim.tbl_deep_extend(
+        "force",
+        {},
+        require("cmp_nvim_lsp").default_capabilities(),
+        lspconfig.util.default_config.capabilities
+      )
+
+
+      vim.lsp.config("*", {
+        capabilities = capabilities,
+        on_attach = require("utils").onAttach,
+      })
+
+      lspconfig["lua_ls"].setup({
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim", "love" },
+            },
+            workspace = {
+              library = {
+                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+                [vim.fn.expand("~/.local/share/love-api")] = true,
+              },
+            },
+          },
+        },
+      })
 
       -- Setup diagnostics
       local diagnostic_config = {
@@ -37,15 +67,17 @@ return {
       end
 
       -- Map the function to a key, e.g., <leader>d
-      vim.api.nvim_set_keymap('n', '<leader>cd', '<cmd>lua Show_line_diagnostics()<CR>', { noremap = true, silent = true, desc = "Show diagnostics" })
+      vim.api.nvim_set_keymap('n', '<leader>cd', '<cmd>lua Show_line_diagnostics()<CR>',
+        { noremap = true, silent = true, desc = "Show diagnostics" })
       -- toggle diagnostic
-        vim.keymap.set('n', '<leader>ct', toggle_diagnostics, { noremap = true, silent = true, desc = "Toggle diagnostics" })
+      vim.keymap.set('n', '<leader>ct', toggle_diagnostics,
+        { noremap = true, silent = true, desc = "Toggle diagnostics" })
     end,
   },
 
   -- mason.nvim
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     config = function()
       require("mason").setup({
         ui = {
@@ -59,45 +91,13 @@ return {
     end,
   },
 
- -- mason-lspconfig.nvim
+  -- mason-lspconfig.nvim old version
   {
-    "williamboman/mason-lspconfig.nvim",
+    "mason-org/mason-lspconfig.nvim",
     config = function()
-      local capabilities = vim.tbl_deep_extend(
-        "force",
-        {},
-        require("cmp_nvim_lsp").default_capabilities(),
-        require("lspconfig").util.default_config.capabilities
-      )
-
-      require("cmp_nvim_lsp").default_capabilities()
       require("mason-lspconfig").setup({
-        automatic_installation = true,
-        handlers = {
-          -- default handlers
-          function(server)
-            require("lspconfig")[server].setup({
-              capabilities = capabilities,
-              on_attach = require("utils").onAttach,
-            })
-          end,
-
-          -- targeted handler
-          ["lua_ls"] = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({
-              capabilities = capabilities,
-              on_attach = require("utils").onAttach,
-              settings = {
-                Lua = {
-                  diagnostics = {
-                    globals = { "vim" },
-                  },
-                },
-              },
-            })
-          end,
-        },
+        ensure_installed = { "lua_ls", "ts_ls", "tailwindcss", "bashls" },
+        automatic_enable = true,
       })
     end,
   },
@@ -105,35 +105,35 @@ return {
   -- neodev.nvim for enhanced Lua development
   { "folke/neodev.nvim", opts = {} },
 
-  	-- set up snippets and completions
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			{ "rafamadriz/friendly-snippets" },
-			{ "L3MON4D3/LuaSnip" },
-			{ "saadparwaiz1/cmp_luasnip" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-		},
-		config = function()
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
+  -- set up snippets and completions
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      { "rafamadriz/friendly-snippets" },
+      { "L3MON4D3/LuaSnip" },
+      { "saadparwaiz1/cmp_luasnip" },
+      { "hrsh7th/cmp-nvim-lsp" },
+    },
+    config = function()
+      local cmp = require("cmp")
+      require("luasnip.loaders.from_vscode").lazy_load()
 
-			cmp.setup({
-				mapping = cmp.mapping.preset.insert({
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "lua_snip" },
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-		end,
-	},
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "lua_snip" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end,
+  },
 }
